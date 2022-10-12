d16d6bfa6c08f4edf111622f17b1c8af6e3fee04
ashgolan <ashgolan@hotmail.com>
2022-10-12 10:58:19 +0300

    19.3 complete

--- javascript/19.3/work.js
+++ javascript/19.3/work.js
@@ -1,10 +1,22 @@
 const password = document.querySelector("p").textContent;
 const inputs = [...document.getElementsByTagName("input")];
+const theCode = document.querySelector("p");
 
-inputs.forEach((element, index) => {
+inputs.forEach((element) => {
   element.maxLength = 1;
   element.onkeyup = function (e) {
     element.textContent = e;
-    if (index != inputs.length - 1) element.nextElementSibling.focus();
+    if (element.nextElementSibling) element.nextElementSibling.focus();
   };
+});
+let copied;
+theCode.addEventListener("copy", (e) => {
+  copied = [...e.target.textContent];
+});
+
+inputs[0].addEventListener("paste", () => {
+  inputs.forEach((element, index) => {
+    element.value = copied[index];
+    element.focus();
+  });
 });
